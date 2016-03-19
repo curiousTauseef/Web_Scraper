8f2e2db62befc4fd2e7b180869cd97b2248ce5c7
sunyam <sunyambagga@gmail.com>
2016-03-19 16:30:43 +0530

    Adding HT

--- News_Scraper.py
+++ News_Scraper.py
@@ -7,7 +7,7 @@ from cookielib import CookieJar
 
 ################################################################################
 # For WashingtonPost
-# Exploiting the property of www.washingtonpost.com- that it encloses the main body of the article in a special <article></article> tag;
+# Article is inside <article></article>
 
 def WashingtonPost(url):
     # Download the URL
@@ -15,8 +15,7 @@ def WashingtonPost(url):
     soup = BeautifulSoup(webpage)
     
     inside_article = str(soup.find_all('article'))
-    # We are inside the <article> </article> tags now
-    # The real info is inside the <p> tags of these <article> tags!
+    # The real info is inside the <p> tags of these <article> tags
     
     soup2 = BeautifulSoup(inside_article, "html.parser")
     #"html.parser" is a crucial argument : http://stackoverflow.com/questions/14822188/dont-put-html-head-and-body-tags-automatically-beautifulsoup
@@ -27,7 +26,7 @@ def WashingtonPost(url):
 
 ################################################################################
 # For TheHindu
-# Exploiting the property- that it encloses the main body of the article in a <p class="body"></p>;
+# Article is inside <p class="body"></p>
 
 def TheHindu(url):
     webpage = urllib2.urlopen(url).read().decode('utf8')
@@ -43,7 +42,7 @@ def TheHindu(url):
 
 ################################################################################
 # For TheNewYorkTimes
-# Exploiting the property that the article is inside <p class="story-body-text story-content"></p>;
+# Article is inside <p class="story-body-text story-content"></p>
 
 def NYtimes(url):
     cj = CookieJar()
@@ -67,7 +66,7 @@ def NYtimes(url):
 def CNN(url):
     webpage = urllib2.urlopen(url).read().decode('utf8')
     soup = BeautifulSoup(webpage)
-    # Article is inside <p> tags for money.cnn
+    # Article is inside <p> tags for money.cnn {except the last few <p> tags}
     if "money.cnn" in url:
         all_p_tags = []
         for tag in soup.findAll("p"):
@@ -81,26 +80,19 @@ def CNN(url):
     elif "edition.cnn" in url:
         article = " ".join(map(lambda x: x.text, soup.find_all("p", {"class" : "zn-body__paragraph"})))
 
-################################################################################
-# Next is for CNN
+    return soup.title.text, article
 
-def CNN(url):
+################################################################################
+# For Hindustan Times
+
+def HindustanTimes(url):
     webpage = urllib2.urlopen(url).read().decode('utf8')
     soup = BeautifulSoup(webpage)
-    # Article is inside <p> tags (except the last <p> tag which isn't useful)
-    # Using simply for loop this time-
-    all_p_tags = []
-    for tag in soup.findAll("p"):
-        all_p_tags.append(tag)
-
-    article = ""
-    for x in all_p_tags[:-2]:
-        article += x.text
+    
+    article = ' '.join(map(lambda x: x.text, soup.findAll("p")))
 
     return soup.title.text, article
 
-
-    return soup.title.text, article
 
 
 
@@ -110,11 +102,14 @@ def CNN(url):
 #url2 = "http://www.thehindu.com/business/budget/highlights-of-union-budget-201617/article8295451.ece?homepage=true"
 #url3 = "http://www.nytimes.com/2016/03/02/technology/apple-and-fbi-face-off-before-house-judiciary-committee.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=first-column-region&region=top-news&WT.nav=top-news"
 #url4 = "http://money.cnn.com/2016/03/09/technology/bolt-electric-bike/index.html"
-url4 = "http://edition.cnn.com/2016/03/14/world/exomars-mars-methane-mission-launch-irpt/index.html"
+#url4 = "http://edition.cnn.com/2016/03/14/world/exomars-mars-methane-mission-launch-irpt/index.html"
+url5 = "http://www.hindustantimes.com/tech/reboot-reality-now-swim-with-sharks-sing-with-paul-mccartney-on-virtual-reality-headsets/story-beTNUkO5kkCxL9tUJIeeOO.html"
+
 #output = WashingtonPost(url1)    # Returns a list of two items
 #output = TheHindu(url2)
 #output = NYtimes(url3)
-output = CNN(url4)
+#output = CNN(url4)
+output = HindustanTimes(url5)
 
 #url = raw_input('Enter a NYtimes url: ')
 
